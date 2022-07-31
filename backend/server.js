02e26bdbeef4359b4550b8ba77a95152522f7acf
Kevin Mason <krmason2020@gmail.com>
2022-07-30 21:12:25 -0700

    created error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const errorHandler = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "welcome to the support desk API" });
+});
+
+//routes, the require returns a router
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
