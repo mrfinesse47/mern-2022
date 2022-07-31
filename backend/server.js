7fea4b2d8b8b81a5f1913d360c88145deab64230
Kevin Mason <krmason2020@gmail.com>
2022-07-30 22:16:01 -0700

    successful connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const errorHandler = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
 const PORT = process.env.PORT || 8000;
+
+//connect to db -- not connecting?? check IP on mongodb atlas or user credentials
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
@@ -17,4 +23,5 @@ app.get("/", (req, res) => {
 //routes, the require returns a router
 app.use("/api/users", require("./routes/userRoutes"));
 
+//http://expressjs.com/en/guide/error-handling.html -- explains error handling
 app.use(errorHandler);
