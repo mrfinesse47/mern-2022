78e9ac65539d11235450107b45fcb51e560e24f9
Kevin Mason <krmason2020@gmail.com>
2022-09-03 14:18:19 -0700

    view all tickets

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import TicketItem from '../components/TicketItem';
 
 const Tickets = () => {
   const { tickets, isLoading, isSuccess } = useSelector(
@@ -19,7 +20,21 @@ const Tickets = () => {
   if (isLoading) {
     return <Spinner />;
   }
-  return <div>ticets</div>;
+  return (
+    <div>
+      <BackButton url={'/'} />
+      <h1>Tickets</h1>
+      <div className='ticket-headings'>
+        <div>Date</div>
+        <div>Product</div>
+        <div>Status</div>
+        <div></div>
+      </div>
+      {tickets.map((ticket) => (
+        <TicketItem key={ticket._id} ticket={ticket} />
+      ))}
+    </div>
+  );
 };
 
 export default Tickets;
